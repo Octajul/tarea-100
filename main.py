cf1888297f212d2a260e592ac1dc016ef6a8e845
Octajul <75392288+Octajul@users.noreply.github.com>
2020-12-03 09:29:29 +0100

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
-# value for the differential between the two sensors.
-# you can change this value to make it more or less sensitive.
+# Variable para llevar a cabo un limite de diferencia mayor entre sensores de luz
 diff = 100
+#Mostrar una imagen de cara dormida
 basic.show_icon(IconNames.ASLEEP)
-input.button_is_pressed(Button.A)
-input.button_is_pressed(Button.B)
+#input.button_is_pressed(Button.A)
+#input.button_is_pressed(Button.B)
+#Parar el bot si estaba en movimiento
 gigglebot.stop()
+#Definir velocidad de movimiento de los motores
 gigglebot.set_speed(gigglebotWhichMotor.BOTH, 50)
+#Loop principal
 while True:
+    #Definir accion si boton A se presiona
     if input.button_is_pressed(Button.A):
+        #Mostrar imagen de corazon
         basic.show_icon(IconNames.HEART)
+        #Entrar en nuevo Loop
         while True:
+            # Mostrar cara feliz
             basic.show_icon(IconNames.HAPPY)
+            #Leer sensores de luz 
             right=gigglebot.light_read_sensor(gigglebotWhichTurnDirection.RIGHT)
             left=gigglebot.light_read_sensor(gigglebotWhichTurnDirection.LEFT)
+            # Ver si un sensor capta mayor luz que el otro 
+            #Si derecha capta mas que izquiera girar a la derecha
             if right > left+diff:
                 gigglebot.turn(gigglebotWhichTurnDirection.RIGHT)
+            #Si capta mas izquierda girar a la izquierda
             elif left > right+diff:
                 gigglebot.turn(gigglebotWhichTurnDirection.LEFT)
+            #Si captan similar ir recto
             else:
                 gigglebot.drive_straight(gigglebotWhichDriveDirection.FORWARD)
+            #Si el boton B es presionado salir del loop y detener el robot y mostrar cara dormida
             if input.button_is_pressed(Button.B):
                 gigglebot.stop()
                 basic.show_icon(IconNames.ASLEEP)
--- main.ts
+++ main.ts
@@ -1,28 +1,41 @@
 let right: number;
 let left: number;
-//  value for the differential between the two sensors.
-//  you can change this value to make it more or less sensitive.
+//  Variable para llevar a cabo un limite de diferencia mayor entre sensores de luz
 let diff = 100
+// Mostrar una imagen de cara dormida
 basic.showIcon(IconNames.Asleep)
-input.buttonIsPressed(Button.A)
-input.buttonIsPressed(Button.B)
+// input.button_is_pressed(Button.A)
+// input.button_is_pressed(Button.B)
+// Parar el bot si estaba en movimiento
 gigglebot.stop()
+// Definir velocidad de movimiento de los motores
 gigglebot.setSpeed(gigglebotWhichMotor.Both, 50)
+// Loop principal
 while (true) {
+    // Definir accion si boton A se presiona
     if (input.buttonIsPressed(Button.A)) {
+        // Mostrar imagen de corazon
         basic.showIcon(IconNames.Heart)
+        // Entrar en nuevo Loop
         while (true) {
+            //  Mostrar cara feliz
             basic.showIcon(IconNames.Happy)
+            // Leer sensores de luz 
             right = gigglebot.lightReadSensor(gigglebotWhichTurnDirection.Right)
             left = gigglebot.lightReadSensor(gigglebotWhichTurnDirection.Left)
+            //  Ver si un sensor capta mayor luz que el otro 
+            // Si derecha capta mas que izquiera girar a la derecha
             if (right > left + diff) {
                 gigglebot.turn(gigglebotWhichTurnDirection.Right)
             } else if (left > right + diff) {
+                // Si capta mas izquierda girar a la izquierda
                 gigglebot.turn(gigglebotWhichTurnDirection.Left)
             } else {
+                // Si captan similar ir recto
                 gigglebot.driveStraight(gigglebotWhichDriveDirection.Forward)
             }
             
+            // Si el boton B es presionado salir del loop y detener el robot y mostrar cara dormida
             if (input.buttonIsPressed(Button.B)) {
                 gigglebot.stop()
                 basic.showIcon(IconNames.Asleep)
